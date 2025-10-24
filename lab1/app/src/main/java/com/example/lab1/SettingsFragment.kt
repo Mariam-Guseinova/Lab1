8d8e29f94b1cf1c7e625cfc71aa621deb3e9fc8e
Mariam-Guseinova <mariam.guseinova.itmo@gmail.com>
2025-10-24 23:32:00 +0300

    design is improved

--- lab1/app/src/main/java/com/example/lab1/SettingsFragment.kt
+++ lab1/app/src/main/java/com/example/lab1/SettingsFragment.kt
@@ -1,26 +1,44 @@
 package com.example.lab1
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.util.Log
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
 
 class SettingsFragment : Fragment() {
-    private val TAG = "SettingsFragmentLifecycle"
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d(TAG, "onCreate: Fragment создан.")
-    }
+    private val TAG = "SettingsFragmentLifecycle"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         Log.d(TAG, "onCreateView: View для фрагмента создается.")
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+
+        val themeSwitch = view.findViewById<Switch>(R.id.theme_switch)
+
+        val isDarkTheme = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
+        themeSwitch.isChecked = isDarkTheme
+
+        themeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+
+        return view
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate: Fragment создан.")
     }
 
     override fun onDestroyView() {
